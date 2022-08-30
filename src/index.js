315282cc46138cb5b25028cbf78c8bff4e9676c5
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-08-30 14:21:33 +0100

    added the cart dynamically and fetch api function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
- import "./scss/main.css";
-import create from "./module/ceateElement.js";
-import {Home} from "./module/Import.js";
+import "./scss/main.css";
+ import getData from "./module/Api.js";
 
-
-const mainDiv = create('div',undefined,Home);
-// Array from API
- const cards = create('div',['cards'],mainDiv);
- cards.id=1;
-const img = create('img',['card-img'],cards);
-img.src = '#';
-img.alt = 'Img';
-const cardBody = create('div',undefined,cards);
-const h1Text = create('h1',undefined,cardBody);
-h1Text.append("first card");
+ getData();
---
+++ src/module/Api.js
@@ -0,0 +1,31 @@
+import create from "./ceateElement.js";
+import {Home} from "./Import.js";
+
+const url = 'https://api.tvmaze.com/shows/1/episodes?specials=1';
+
+const createcard =  (data) =>{
+    const mainDiv = create('div',undefined,Home);
+     [...data].forEach(el => {
+        if(el.image['medium'] !== null)
+        {
+            const cards = create('div', ['cards'], mainDiv);
+            cards.id = el.id;
+            const img = create('img', ['card-img'], cards);
+            img.src = el.image['medium'];
+            img.alt = el.name;
+            const cardBody = create('div', undefined, cards);
+            const h1Text = create('h3', undefined, cardBody);
+            h1Text.append(el.name);
+        }
+    });
+}
+
+const getData = async () => {
+    await fetch(url)
+   .then((response) => response.json())
+   .then((data)=>{
+      createcard(data);
+   })
+};
+
+export  default getData;
