e909ea157d043cae140f18a084cffb179f551947
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-09-01 16:59:46 +0100

    added the final styling of the pupup and did the request test for show comment and get comment

--- src/module/test.test.js
+++ src/module/test.test.js
@@ -2,3 +2,38 @@ const array = ['sdsdsdss', 'sdsdsdsd', 'sdsdsd'];
 
 
 const comment = (comm) => comm.length;
+
+
+// testing the requestcomment
+//const url = '';
+// const postComment = async (data) => {
+//     await fetch(url, {
+//       method: 'POST',
+//       body: JSON.stringify(data),
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Accept: 'application/json',
+//       },
+//     });
+//   };
+  
+//   const getComments = async (id) => {
+//     const getUrl = `?item_id=${id}`;
+//     const response = await fetch(url + getUrl);
+//     const data = await response.json();
+//     return data;
+//   };
+  
+//   export { postComment, getComments };
+
+
+// testing the git all data
+
+// const getAllData = async () => {
+//     const response = await fetch('https://api.tvmaze.com/shows');
+//     const getJsonObj = await response.json();
+//     const result = getJsonObj.slice(0, 18);
+//     return result.length;
+//   };
+  
+//   module.exports = getAllData;
